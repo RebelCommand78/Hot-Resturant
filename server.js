dbf809bce62f895e37adc043a5523cb3dee9f2aa
Chris Neal <voodoodaddy2000@gmail.com>
2019-11-13 13:35:03 -0700

    Reading in Res and building table list

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [];
+var tableRes = [];
+var waitList = [];
 
 // Routes
 // =============================================================
@@ -32,6 +33,22 @@ app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/reserve.html"));
 });
 
+app.post("/api/tables", function(req, res) {
+  //console.log(req.body)
+  const newTable = req.body;
+  if(tableRes.length < 5){
+    tableRes.push(newTable);
+  }
+  else{
+    waitList.push(newTable);
+  }
+  //console.log(tableRes)
+  res.json(newTable);
+});
+
+app.get("/api/tables", function(req, res) {
+  res.json(tableRes);
+});
 
 // Starts the server to begin listening
 // =============================================================
